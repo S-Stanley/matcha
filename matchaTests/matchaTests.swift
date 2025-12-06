9e8ecb97397c218ba305892930722e78359c6d15
Nathan <nathan.bechon@free.fr>
2025-12-06 16:11:40 +0100

    Initial Commit

---
+++ matchaTests/matchaTests.swift
@@ -0,0 +1,17 @@
+//
+//  matchaTests.swift
+//  matchaTests
+//
+//  Created by Nathan Bechon on 06/12/2025.
+//
+
+import Testing
+@testable import matcha
+
+struct matchaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
