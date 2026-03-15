8f32dbabbb2e54b3ad2757ca3fdf513bd3e3d3e7
S-Stanley <stanleyserbin@gmail.com>
2026-03-15 20:42:32 +0100

    feat(databae): 500 default users

--- database/fixtures.sql
+++ database/fixtures.sql
@@ -32,3 +32,48 @@ INSERT INTO "User" (
   '$2b$12$F5aC/GukiNJVheLdQMss4.wJQrpo.m8y.jOKia.Vf6buBks22hJu.',
   '5fb9b8f1-8b36-445e-a6c0-a71b1a1efaa2'
 );
+
+
+INSERT INTO "User" (
+  id,
+  email,
+  username,
+  firstname,
+  lastname,
+  age,
+  city,
+  gender,
+  preference,
+  popularity,
+  password,
+  token
+)
+SELECT
+  id,
+  email,
+  username,
+  firstname,
+  lastname,
+  age,
+  city,
+  gender,
+  preference,
+  popularity,
+  password,
+  token
+FROM (
+  SELECT
+    gen_random_uuid() as id,
+    'user-' || item || '@matcha.com' as email,
+    'user-' || item as username,
+    (ARRAY['Lea', 'Thomas', 'Lucas', 'Manon'])[ceil(random() * 3.99)] AS firstname,
+    (ARRAY['Martin', 'Bernard', 'Petit', 'Dubois'])[ceil(random() * 3.99)] AS lastname,
+    (ARRAY['Paris', 'Marseille', 'Toulouse', 'Lyon', 'Bordeaux', 'Strasbourg', 'Montpellier', 'Nice', 'Ajaccio', 'Cayenne', 'Fort-de-France'])[ceil(random() * 10.99)] AS city,
+    (SELECT ARRAY_AGG(n) FROM generate_series(18, 99) as n)[ceil(random() * 76.99)] as age,
+    (ARRAY['MALE', 'FEMALE', 'DO NOT PRONONCE', 'OTHERS'])[ceil(random() * 3.99)]::gender AS gender,
+    (ARRAY['MALE', 'FEMALE', 'BOTH'])[ceil(random() * 2.99)]::preference AS preference,
+    random() * 100 as popularity,
+    'hXgGAY2u4XNu/UpAcqHjOuyytNs.DvXf0iLE0.5R6OiFh7bMaH21q' as password, -- pass
+    gen_random_uuid() as token
+  FROM generate_series(1, 20) AS item
+) as sub
